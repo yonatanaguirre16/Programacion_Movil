59d89883cf4913d29fbfce90c0450019a5cac4b7
yonatanaguirre16 <yaguirre_16@alu.uabcs.mx>
2026-03-11 08:28:49 -0700

    ejercicio validaciones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/todoapp/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/todoapp/screens/SignUpScreen.kt
@@ -1,4 +1,5 @@
 package com.example.todoapp.screens
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,14 +9,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,6 +32,25 @@ import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun SignUpScreen(navController: NavHostController, modifier: Modifier = Modifier) {
+
+    // ESTADO
+
+    var nombre by remember { mutableStateOf("") }
+    var nombreError by remember { mutableStateOf(false) }
+
+    var email by remember { mutableStateOf("") }
+    var mailError by remember { mutableStateOf(false) }
+
+    var telefono by remember { mutableStateOf("") }
+    var telError by remember { mutableStateOf(false) }
+
+    var pass by remember { mutableStateOf("") }
+    var passError by remember { mutableStateOf(false) }
+
+
+    fun isValidEmail(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
 
     Box(
         modifier = modifier
@@ -41,42 +68,84 @@ fun SignUpScreen(navController: NavHostController, modifier: Modifier = Modifier
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-            Text("Register Screen", fontSize = 30.sp)
+            Text("Crear Cuenta", fontSize = 30.sp)
 
             Spacer(modifier = Modifier.height(20.dp))
 
+            // FIELD NOMBRE
             OutlinedTextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("User") },
+                value = nombre,
+                onValueChange = {
+                    if (it.all { char -> char.isLetter()}) {
+                        nombre = it
+                        nombreError = it.isBlank()
+                    }
+                },
+                label = { Text("Nombre") },
                 modifier = Modifier.fillMaxWidth(),
+                isError = nombreError,
+                supportingText = {
+                    if (nombreError) {
+                        Text("El nombre solo debe contener letras")
+                    }
+                },
+                singleLine = true,
             )
 
             Spacer(modifier = Modifier.height(10.dp))
 
+            // FIELD EMAIL
             OutlinedTextField(
-                value = "",
-                onValueChange = {},
+                value = email,
+                onValueChange = {
+                    email = it
+                    mailError = !isValidEmail(email = it)
+                },
+                label = { Text("Correo") },
+                isError = mailError,
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // FIELD TELEFONO
+            OutlinedTextField(
+                value = telefono,
+                onValueChange = {
+                    if (it.all { char -> char.isDigit() }) {
+                        telefono = it
+                    }
+                },
                 label = { Text("Teléfono") },
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
             )
 
             Spacer(modifier = Modifier.height(10.dp))
 
+            // FIELD PASS
             OutlinedTextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("Password") },
-                modifier = Modifier.fillMaxWidth()
+                value = pass,
+                onValueChange = { pass = it },
+                label = { Text("Contraseña") },
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
             )
 
             Spacer(modifier = Modifier.height(10.dp))
 
+            // FIELD CONFIRM PASS
             OutlinedTextField(
                 value = "",
                 onValueChange = {},
-                label = { Text("ConfirmPassword") },
-                modifier = Modifier.fillMaxWidth()
+                label = { Text("Confirmar Contraseña") },
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
             )
 
             Spacer(modifier = Modifier.height(10.dp))
@@ -98,6 +167,5 @@ fun SignUpScreen(navController: NavHostController, modifier: Modifier = Modifier
 @Preview(showBackground = true)
 @Composable
 fun SignUpPreview() {
-        SignUpScreen(rememberNavController())
-    
+    SignUpScreen(rememberNavController())
 }
